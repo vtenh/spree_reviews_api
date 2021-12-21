7dc444826354d260c8f8c928b145f09149f1b84d
engpolyden <polydeneng@gmail.com>
2021-12-21 10:22:31 +0700

    Close #29 fix wrong scoping review when create feedback

--- app/models/spree_reviews_api/spree/feedback_review_decorator.rb
+++ app/models/spree_reviews_api/spree/feedback_review_decorator.rb
@@ -2,9 +2,17 @@ module SpreeReviewsApi
   module Spree
     module FeedbackReviewDecorator
       def self.prepended(base)
+        base.validate :validate_own_review_feedback
+
         base.after_save :recalculate_review_feedback
 
         base.belongs_to :review, dependent: :destroy, counter_cache: true
+      end
+
+      def validate_own_review_feedback
+        if user_id == review.user_id
+          errors.add(:user_id, 'Unable to give feedback on your own review!')
+        end
       end
 
       def recalculate_review_feedback
